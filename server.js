4c55c3b4d432c3e418da5b7b5aee7ce34df855bb
pc <ammar.futur@gmail.com>
2024-02-26 23:08:55 +0100

    Creating the server, structuring architecture of project

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+
+const PORT = process.env.PORT || 2020 ;
+
+//=========Middleware========
+app.use(morgan("dev"));
+
+//Server :
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+
+console.log(app);
+
+
+
+
+
+
+
+
