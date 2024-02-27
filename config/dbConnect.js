7197960c9c79eb2ac26e4304a53fcfd458b883a4
pc <ammar.futur@gmail.com>
2024-02-27 20:34:49 +0100

    Environement Variables ,dotenv package...

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const dbConnection = async() => {
     try{
-        await mongoose.connect("mongodb+srv://admin:admin@cluster0.psar3hp.mongodb.net/NodeJsSchoolManagement?retryWrites=true&w=majority&appName=Cluster0");
-        console.log('DB Conection succcessfully.')
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('DB Conection succcessfully.');
     }
     catch(error)
            {
