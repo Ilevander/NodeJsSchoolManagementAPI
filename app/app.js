1470344a9f61c44b5838ca0009f124a35188bae1
pc <ammar.futur@gmail.com>
2024-02-28 14:21:04 +0100

    Middleware implementation and calling into server

--- app/app.js
+++ app/app.js
@@ -10,6 +10,46 @@ const app = express();
 app.use(morgan("dev"));
 app.use(express.json()); //pass incoming json data
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.originalUrl}`);
+    next();
+});
+
+//EXEMPLE AND IMPLEMENTATION: 
+let user = {
+    name: "Ilyass",
+    isAdmin: false,
+    isLogin: false, //if false:calling isLogin Middleware || if true:calling the isAdmin Middleware
+};
+
+const isLogin = (req,res,next) => {
+    if(user.isLogin)
+       {
+        next();
+       }
+       else
+            {
+                res.status(401).json({
+                    msg: "Unautorized",
+                });
+            }
+};
+
+const isAdmin = (req,res,next) => {
+    if(user.isAdmin)
+       {
+        next();
+       }
+       else
+            {
+                res.status(401).json({
+                    msg: "Unautorized , you are not admin",
+                });
+            }
+};
+//calling and using Middleware to SERVER:
+app.use(isLogin,isAdmin);
+
 // ===== USING Routes Express =====:
 
 //POST : Admin Registration
