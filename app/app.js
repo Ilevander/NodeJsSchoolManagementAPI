cdc10d951fae90a11ab055d8800a6f24c0ba8f1a
dell <dell@DESKTOP-4P8U8AR>
2024-03-04 02:59:10 +0100

    post academicYear tested at postman

--- app/app.js
+++ app/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 
 const adminRouter = require("../routes/staff/adminRouter");
+const academicYearRouter = require('../routes/academics/academicYear');
+
 const {globalErrorHandler, notFoundError} = require('../middlewares/globalErrorHandler');
+
 
 const app = express();
 
@@ -13,11 +16,12 @@ app.use(express.json()); //pass incoming json data
 
 // ===== USING Routes Express =====:
 app.use("/api/v1/admins", adminRouter);
+app.use("/api/v1/academic-years",academicYearRouter);
 
 //Error middlewares : 
 app.use(notFoundError);
 app.use(globalErrorHandler);
 
-
+ 
 module.exports = app;
 
